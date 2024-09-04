472c51758e2dfe1afcc9ef039d894e9d933f174b
MatheusESMelo <matheus.estevesmelo@gmail.com>
2024-09-04 15:56:51 -0300

    Creating app for chat and frontend.

--- chat_app/chat_app/asgi.py
+++ chat_app/chat_app/asgi.py
@@ -1,9 +1,8 @@
 import os
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-from django.urls import path
-# Import your consumers here when created
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
 
@@ -11,9 +10,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            [
-                # Add WebSocket routes here
-            ]
+            websocket_urlpatterns
         )
     ),
 })
--- chat_app/chat_app/settings.py
+++ chat_app/chat_app/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework',
-    'channels',
+    "rest_framework",
+    "channels",
     "corsheaders",
+    "users",
+    "chat",
 ]
 
 MIDDLEWARE = [
